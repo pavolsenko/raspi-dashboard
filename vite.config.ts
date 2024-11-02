13fd322906d2f39495a9d1488760b1a99b57abda
Pavol Senko <pavol.senko@gmail.com>
2024-11-02 16:52:18 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default ({ mode }: { mode: string }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: 'build',
+    },
+    cacheDir: 'node_modules',
+});
 
-    return defineConfig({
-        plugins: [react()],
-        build: {
-            outDir: 'build',
-        },
-        cacheDir: 'node_modules',
-    });
-};
